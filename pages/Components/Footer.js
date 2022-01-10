9387f1c03eba4a4fe3942eaca39c7d733dfc3069
aashankhan2981 <aashankhan2981@gmail.com>
2022-01-10 20:22:11 +0500

    commit

--- pages/Components/Footer.js
+++ pages/Components/Footer.js
@@ -1,85 +1,115 @@
-import React from 'react'
-import CaseStudies from './CaseStudies'
-import FotterGallery from './FotterGallery'
-import SecondPara from './SecondPara'
+import React from "react";
+import CaseStudies from "./CaseStudies";
+import FotterGallery from "./FotterGallery";
+import SecondPara from "./SecondPara";
 
 const Footer = () => {
-    const num = [1,2,3,4]
-    return (
-        <div className=''>
-            <div className='relative bottom-36   z-10 flex flex-col items-center justify-center px-[148px] gap-8'>
-                <div className='mb-8'>
-                <img src="./svgs/divfoot1.svg" alt="foot-div" />
-                </div>
-               
+  const num = [1, 2, 3, 4];
+  return (
+    <div className="">
+      <div className="relative bottom-36   z-10 flex flex-col items-center justify-center px-[148px] gap-8">
+        <div className="mb-8">
+          <img src="./svgs/divfoot1.svg" alt="foot-div" />
+        </div>
+      </div>
+      <div className="relative  bg-black   flex flex-col  items-center  justify-center  ">
+        <div className="flex px-[148px] relative bottom-36 flex-col  gap-8">
+          <div className="">
+            <img src="./svgs/divfoot2.svg" alt="foot-div" />
+          </div>
+          <div>
+            <img src="./svgs/divfoot3.svg" alt="foot-div" />
+          </div>
+        </div>
+        <div className="relative bottom-12 ">
+          <SecondPara
+            black={true}
+            title={"The balance of forces."}
+            desc={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a dolor nulla nulla quam pellentesque risus consequat est. Feugiat vulputate sapien enim neque non blandit ullamcorper sit. Eget id augue mi nisi quam turpis morbi. "
+            }
+          />
+        </div>
+        <div className="flex gap-8 px-[148px] mb-24">
+          <FotterGallery
+            src1={"./svgs/footergallery1.svg"}
+            src2={"./svgs/footerframe2.svg"}
+          />
+          <FotterGallery
+            src1={"./svgs/footerframe2.svg"}
+            src2={"./svgs/footergallery1.svg"}
+          />
+        </div>
+      </div>
+      <div className="bg-black pb-10 px-[148px] flex flex-col items-start">
+        <SecondPara
+          title={"Very soflty."}
+          desc={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a dolor nulla nulla quam pellentesque risus consequat est. Feugiat vulputate sapien enim neque non blandit ullamcorper sit. Eget id augue mi nisi quam turpis morbi. "
+          }
+          black={true}
+        />
+        <div className="flex gap-8 my-24">
+          {num.map((num, index) => {
+            return (
+              <div key={index}>
+                <img
+                  src="./svgs/Footsecondframe.svg"
+                  key={num}
+                  alt="footer-div"
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <h2 className="text-white font-medium text-[54px]">
+            More Case Studies
+          </h2>
+          <div className="flex gap-8 mt-12 mb-32">
+            <CaseStudies
+              title={"Toyota North America"}
+              desc={"UX optimastion increasing conversion by 30%"}
+            />
+            <CaseStudies
+              title={"SCTE-ISBE"}
+              desc={"Systems Architecture using micro-services technology"}
+            />
+            <CaseStudies
+              title={"Toyota North America"}
+              desc={"UX optimastion increasing conversion by 30%"}
+            />
+          </div>
+        </div>
+        {/* <div className="relative left-96">
+          <img src="./svgs/footarrow.svg" alt="arrow" />
+        </div> */}
+        <div className="flex items-center justify-start">
+          <div className="flex flex-col gap-14">
+            <div>
+              <h3 className="font-normal text-[40px] leading-tight text-white">
+                Are you ready?{" "}
+              </h3>
+              <span className="font-normal text-[40px] text-white leading-tight opacity-30">
+                because we are
+              </span>
             </div>
-            <div className='relative  bg-black   flex flex-col  items-center  justify-center  '>
-            <div className='flex px-[148px] relative bottom-36 flex-col  gap-8'>
-            <div className=''>
-                <img src="./svgs/divfoot2.svg" alt="foot-div" />
-                </div>
-                <div >
-                <img src="./svgs/divfoot3.svg" alt="foot-div" />
-                </div>
-            </div>
-            <div className='relative bottom-12 '>
-            <SecondPara black={true} title={"The balance of forces."} desc={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a dolor nulla nulla quam pellentesque risus consequat est. Feugiat vulputate sapien enim neque non blandit ullamcorper sit. Eget id augue mi nisi quam turpis morbi. "} />
-            </div>
-            <div className='flex gap-8 px-[148px] mb-24'>
-            <FotterGallery src1={"./svgs/footergallery1.svg"} src2={"./svgs/footerframe2.svg"} />
-            <FotterGallery src1={"./svgs/footerframe2.svg"} src2={"./svgs/footergallery1.svg"} />
-            </div>
-            </div >
-            <div className='bg-black pb-10 px-[148px] flex flex-col items-start'>
-                <SecondPara title={"Very soflty."} desc={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a dolor nulla nulla quam pellentesque risus consequat est. Feugiat vulputate sapien enim neque non blandit ullamcorper sit. Eget id augue mi nisi quam turpis morbi. "} black={true}/>
-                <div className='flex gap-8 my-24'>
-                    {num.map((num,index)=>{
-                        return  ( <div key={index} >
-                        <img src="./svgs/Footsecondframe.svg" key={num} alt="footer-div" />
-                    </div>)
-                    })}
-              
-               
-                </div>
-                <div>
-                    <h2 className='text-white font-medium text-[54px]'>More Case Studies</h2>
-                    <div className='flex gap-8 mt-12 mb-32'>
-               
-                    <CaseStudies title={"Toyota North America"} desc={"UX optimastion increasing conversion by 30%"}/>
-                    <CaseStudies title={"SCTE-ISBE"} desc={"Systems Architecture using micro-services technology"}/>
-                    <CaseStudies title={"Toyota North America"} desc={"UX optimastion increasing conversion by 30%"}/>
-                    </div>
-
-                </div>
-                <div className='relative left-96'>
-                <img src="./svgs/footarrow.svg"   alt="arrow" />
-
-                    </div>
-                <div className='flex items-center justify-start'>
-                    <div className='flex flex-col gap-14'>
-                        <div>
-                        <h3 className='font-normal text-[40px] leading-tight text-white'>Are you ready? </h3>
-                        <span className='font-normal text-[40px] text-white leading-tight opacity-30'>because we are</span>
-                        </div>
-                        <div className='flex  items-center gap-1'>
-                <h2 className='text-2xl font-normal border-b-[1px] border-b-white  text-white'>Get in touch</h2>
-                {/* <div>
+            <div className="flex  items-center gap-1">
+              <h2 className="text-2xl font-normal border-b-[1px] border-b-white  text-white">
+                Get in touch
+              </h2>
+              <div>
                 <img src="./svgs/footerarrowlast.svg" alt="arrow" />
-                </div> */}
+              </div>
             </div>
             <div>
-            <img src="./svgs/alpha.svg" alt="logo" />
+              <img src="./svgs/alpha.svg" alt="logo" />
             </div>
-
-                    </div>
-                 
-
-
-                </div>
-            </div>
-
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ pages/Components/Lastfoot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lastfoot = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Lastfoot
